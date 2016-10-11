a5417011f124c04d39a0c7b3974c8131e01f3960
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-11 14:43:39 +0300

    section 41 complete

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -2,15 +2,16 @@
  * Created by ivanlazarev on 11.10.16.
  */
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
     renderList() {
         return this.props.books.map((book) => {
             return (
                 <li key={book.title} className="list-group-item">{book.title}</li>
             );
         });
-    }
+    };
 
     render() {
         return (
@@ -18,5 +19,13 @@ export default class BookList extends Component {
                 {this.renderList()}
             </ul>
         );
-    }
+    };
 }
+
+function mapStateToProps(state) { //if 'state' changes all will automatically re renders
+    return {
+        books: state.books // grabs to this.props to component
+    };
+}
+
+export default connect(mapStateToProps)(BookList); // produces container
