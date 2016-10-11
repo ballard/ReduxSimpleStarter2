53796f2f35bc3607151a7f9730689a6d58529ba9
Ivan Lazarev <ivan.lazarev@sarov-itc.ru>
2016-10-11 11:33:46 +0300

    section 38 complete

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ivanlazarev on 11.10.16.
+ */
+export default function () {
+    return [
+        {title: 'Javascript: The Good Parts' },
+        {title: 'Harry Potter'},
+        {title: 'The Dark Tower'},
+        {title: 'Eloquent Ruby'}
+    ]
+}
